34b14a32fcf5cf58ce0d59e96eb645b5c9ef9bcd
wangxing <861845012@qq.com>
2017-10-29 20:43:28 +0800

    登录页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,25 @@
 /**
  * Created by Administrator on 2017-10-29.
  */
+//ajax启动到加载完成的进度条
+// $(document).ajaxStart(function () {
+//   NProgress.start();
+// });
+// $(document).ajaxStop(function () {
+//   setTimeout(function () {
+//     NProgress.done();
+//   }, 600);
+// });
+
+$(document).ajaxStart(function () {
+  //让进度条显示出来
+  NProgress.start();
+})
+
+
+$(document).ajaxStop(function () {
+  setTimeout(function () {
+    //让进度条结束
+    NProgress.done();
+  }, 500);
+});
